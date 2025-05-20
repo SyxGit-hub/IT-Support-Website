f457f5967f88653671e09da54069fd800ad97f2e
Felix <f.leipert@ppc-ag.de>
2025-05-20 12:04:52 +0200

    added css js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+
+    // Schließen des Menüs beim Klick auf einen Link
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+            hamburger.classList.remove('active');
+        });
+    });
+}); 
